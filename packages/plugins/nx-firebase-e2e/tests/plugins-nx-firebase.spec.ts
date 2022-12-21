5e3eef3d31ac041073264d85f6801e0e16179efc
Bullfrog1234-Spectre <anthony@penguins.red>
2022-12-22 00:17:07 +1100

    add plugin library nx-firebase

---
+++ packages/plugins/nx-firebase-e2e/tests/plugins-nx-firebase.spec.ts
@@ -0,0 +1,64 @@
+import {
+  checkFilesExist,
+  ensureNxProject,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+
+describe('plugins-nx-firebase e2e', () => {
+  // Setting up individual workspaces per
+  // test can cause e2e runs to take a long time.
+  // For this reason, we recommend each suite only
+  // consumes 1 workspace. The tests should each operate
+  // on a unique project in the workspace, such that they
+  // are not dependant on one another.
+  beforeAll(() => {
+    ensureNxProject(
+      '@red-penguins/nx-firebase',
+      'dist/packages/plugins/nx-firebase'
+    );
+  });
+
+  afterAll(() => {
+    // `nx reset` kills the daemon, and performs
+    // some work which can help clean up e2e leftovers
+    runNxCommandAsync('reset');
+  });
+
+  it('should create plugins-nx-firebase', async () => {
+    const project = uniq('plugins-nx-firebase');
+    await runNxCommandAsync(
+      `generate @red-penguins/nx-firebase:plugins-nx-firebase ${project}`
+    );
+    const result = await runNxCommandAsync(`build ${project}`);
+    expect(result.stdout).toContain('Executor ran');
+  }, 120000);
+
+  describe('--directory', () => {
+    it('should create src in the specified directory', async () => {
+      const project = uniq('plugins-nx-firebase');
+      await runNxCommandAsync(
+        `generate @red-penguins/nx-firebase:plugins-nx-firebase ${project} --directory subdir`
+      );
+      expect(() =>
+        checkFilesExist(`libs/subdir/${project}/src/index.ts`)
+      ).not.toThrow();
+    }, 120000);
+  });
+
+  describe('--tags', () => {
+    it('should add tags to the project', async () => {
+      const projectName = uniq('plugins-nx-firebase');
+      ensureNxProject(
+        '@red-penguins/nx-firebase',
+        'dist/packages/plugins/nx-firebase'
+      );
+      await runNxCommandAsync(
+        `generate @red-penguins/nx-firebase:plugins-nx-firebase ${projectName} --tags e2etag,e2ePackage`
+      );
+      const project = readJson(`libs/${projectName}/project.json`);
+      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+    }, 120000);
+  });
+});
---
+++ packages/plugins/nx-firebase/src/generators/plugins-nx-firebase/schema.d.ts
@@ -0,0 +1,5 @@
+export interface PluginsNxFirebaseGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
